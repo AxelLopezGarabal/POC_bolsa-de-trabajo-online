cbaaf6bba3a47faef4aba0a34fdecb4563d5676e
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2021-03-09 23:44:57 -0300

    pass backend from js to php

--- src/backend/model/student/Career.js
+++
@@ -1,8 +0,0 @@
-
-const career = {
-	NAVY_ARQ: "Arquitectura Naval",
-	LI: "Licenciatura Informatica",
-	TPI: "Tecnicatura Programacion Informatica"
-}
-
-module.exports = career; 
--- src/backend/model/student/TypeOfDocument.js
+++
@@ -1,6 +0,0 @@
-const type = {
-	ARG:"DNI",
-    PAR:"Cedula de identidad"
-}
-
-module.exports = type; 
